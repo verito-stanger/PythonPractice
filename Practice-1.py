19b5b550f2a9924067b3993195c74cf017056aeb
Mervin <mervindiazlugo>
2020-10-06 20:52:50 -0300

    modified Practice-1

--- Practice-1.py
+++ Practice-1.py
@@ -1,11 +1,11 @@
-"""
+
 a = 50
 b ='100.1'
 print(a+int(a))  
 
 print(type(a))
 print(type(b))
-"""
+
 array = [23, 4 , 'Vero', 12.3,{'Vero': '95802122', 'Mikey': '19092838'}] 
 print(array[4]['Mikey'])
 array.append(2)
