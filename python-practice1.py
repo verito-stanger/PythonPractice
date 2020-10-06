b9c94f906e236376b288cabafcdb4c48ce8c8454
Serrano Diaz <v.a.serrano.diaz@accenture.com>
2020-10-06 20:39:18 -0300

    First commit

---
+++ python-practice1.py
@@ -0,0 +1,12 @@
+#My First Program
+#This program says Hello and asks for my name.
+
+print('Hola Mundo!')
+print('Cual es tu nombre?') #asks for your name
+myName = input()
+print('Holaa! '+ myName + '  Como estás?')
+saludo = input()
+print('Me alegra que estés ' + saludo + ' ,tengo un mensaje para vos! deseas leerlo?')
+yes =input()
+print ('El mensaje es que: VERO TE ADORAAA!!')
+
