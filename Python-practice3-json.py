a959a3dbbbf8f68a4125f7281b3aa2334b7f3c16
Serrano Diaz <v.a.serrano.diaz@accenture.com>
2020-10-06 21:54:42 -0300

    Exercise class 2 + homework

---
+++ Python-practice3-json.py
@@ -0,0 +1,312 @@
+responseTwitter= {
+   "statuses": [
+      {
+         "created_at": "Thu Oct 01 14:18:34 +0000 2020",
+         "id": 1311671856303411202,
+         "id_str": "1311671856303411202",
+         "text": "I GOT THE SLEAZE IN MY TEETH, WHEN I MOVE I'M SLICK, NOTHING EVER STICKS TO ME... \n\nhttps://t.co/ESGts1XAgW\u2026 https://t.co/0sAULwV2kp",
+         "truncated": true,
+         "entities": {
+            "hashtags": [],
+            "symbols": [],
+            "user_mentions": [],
+            "urls": [
+               {
+                  "url": "https://t.co/ESGts1XAgW",
+                  "expanded_url": "https://stevenwilson.lnk.to/ESSo/Youtube",
+                  "display_url": "stevenwilson.lnk.to/ESSo/Youtube",
+                  "indices": [
+                     84,
+                     107
+                  ]
+               },
+               {
+                  "url": "https://t.co/0sAULwV2kp",
+                  "expanded_url": "https://twitter.com/i/web/status/1311671856303411202",
+                  "display_url": "twitter.com/i/web/status/1\u2026",
+                  "indices": [
+                     109,
+                     132
+                  ]
+               }
+            ]
+         },
+         "metadata": {
+            "result_type": "popular",
+            "iso_language_code": "en"
+         },
+         "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+         "in_reply_to_status_id": null,
+         "in_reply_to_status_id_str": null,
+         "in_reply_to_user_id": null,
+         "in_reply_to_user_id_str": null,
+         "in_reply_to_screen_name": null,
+         "user": {
+            "id": 617155406,
+            "id_str": "617155406",
+            "name": "Steven Wilson",
+            "screen_name": "StevenWilsonHQ",
+            "location": "",
+            "description": "This is the official Twitter of musician, singer, songwriter and record producer Steven Wilson.",
+            "url": "https://t.co/G1ZbAQ0izd",
+            "entities": {
+               "url": {
+                  "urls": [
+                     {
+                        "url": "https://t.co/G1ZbAQ0izd",
+                        "expanded_url": "http://stevenwilsonhq.com",
+                        "display_url": "stevenwilsonhq.com",
+                        "indices": [
+                           0,
+                           23
+                        ]
+                     }
+                  ]
+               },
+               "description": {
+                  "urls": []
+               }
+            },
+            "protected": false,
+            "followers_count": 87118,
+            "friends_count": 25,
+            "listed_count": 762,
+            "created_at": "Sun Jun 24 14:14:45 +0000 2012",
+            "favourites_count": 4,
+            "utc_offset": null,
+            "time_zone": null,
+            "geo_enabled": false,
+            "verified": true,
+            "statuses_count": 1801,
+            "lang": null,
+            "contributors_enabled": false,
+            "is_translator": false,
+            "is_translation_enabled": false,
+            "profile_background_color": "0A0A0A",
+            "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+            "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+            "profile_background_tile": false,
+            "profile_image_url": "http://pbs.twimg.com/profile_images/1238011880582586368/pN69tR68_normal.jpg",
+            "profile_image_url_https": "https://pbs.twimg.com/profile_images/1238011880582586368/pN69tR68_normal.jpg",
+            "profile_banner_url": "https://pbs.twimg.com/profile_banners/617155406/1591372249",
+            "profile_link_color": "111111",
+            "profile_sidebar_border_color": "C0DEED",
+            "profile_sidebar_fill_color": "DDEEF6",
+            "profile_text_color": "333333",
+            "profile_use_background_image": true,
+            "has_extended_profile": false,
+            "default_profile": false,
+            "default_profile_image": false,
+            "following": null,
+            "follow_request_sent": null,
+            "notifications": null,
+            "translator_type": "none"
+         },
+         "geo": null,
+         "coordinates": null,
+         "place": null,
+         "contributors": null,
+         "is_quote_status": false,
+         "retweet_count": 25,
+         "favorite_count": 212,
+         "favorited": false,
+         "retweeted": false,
+         "possibly_sensitive": false,
+         "lang": "en"
+      },
+      {
+         "created_at": "Tue Sep 29 10:58:37 +0000 2020",
+         "id": 1310896758814396416,
+         "id_str": "1310896758814396416",
+         "text": "EMINENT SLEAZE - https://t.co/DmhTJv1JBY\n\n\ud83d\udcf7 by Andrew Hobbs https://t.co/CWbkFg3sNu",
+         "truncated": false,
+         "entities": {
+            "hashtags": [],
+            "symbols": [],
+            "user_mentions": [],
+            "urls": [
+               {
+                  "url": "https://t.co/DmhTJv1JBY",
+                  "expanded_url": "https://stevenwilson.lnk.to/ESSo",
+                  "display_url": "stevenwilson.lnk.to/ESSo",
+                  "indices": [
+                     17,
+                     40
+                  ]
+               }
+            ],
+            "media": [
+               {
+                  "id": 1310896723758325760,
+                  "id_str": "1310896723758325760",
+                  "indices": [
+                     60,
+                     83
+                  ],
+                  "media_url": "http://pbs.twimg.com/media/EjE9n_TWoAA-DUd.jpg",
+                  "media_url_https": "https://pbs.twimg.com/media/EjE9n_TWoAA-DUd.jpg",
+                  "url": "https://t.co/CWbkFg3sNu",
+                  "display_url": "pic.twitter.com/CWbkFg3sNu",
+                  "expanded_url": "https://twitter.com/StevenWilsonHQ/status/1310896758814396416/photo/1",
+                  "type": "photo",
+                  "sizes": {
+                     "thumb": {
+                        "w": 150,
+                        "h": 150,
+                        "resize": "crop"
+                     },
+                     "large": {
+                        "w": 2048,
+                        "h": 2048,
+                        "resize": "fit"
+                     },
+                     "small": {
+                        "w": 680,
+                        "h": 680,
+                        "resize": "fit"
+                     },
+                     "medium": {
+                        "w": 1200,
+                        "h": 1200,
+                        "resize": "fit"
+                     }
+                  }
+               }
+            ]
+         },
+         "extended_entities": {
+            "media": [
+               {
+                  "id": 1310896723758325760,
+                  "id_str": "1310896723758325760",
+                  "indices": [
+                     60,
+                     83
+                  ],
+                  "media_url": "http://pbs.twimg.com/media/EjE9n_TWoAA-DUd.jpg",
+                  "media_url_https": "https://pbs.twimg.com/media/EjE9n_TWoAA-DUd.jpg",
+                  "url": "https://t.co/CWbkFg3sNu",
+                  "display_url": "pic.twitter.com/CWbkFg3sNu",
+                  "expanded_url": "https://twitter.com/StevenWilsonHQ/status/1310896758814396416/photo/1",
+                  "type": "photo",
+                  "sizes": {
+                     "thumb": {
+                        "w": 150,
+                        "h": 150,
+                        "resize": "crop"
+                     },
+                     "large": {
+                        "w": 2048,
+                        "h": 2048,
+                        "resize": "fit"
+                     },
+                     "small": {
+                        "w": 680,
+                        "h": 680,
+                        "resize": "fit"
+                     },
+                     "medium": {
+                        "w": 1200,
+                        "h": 1200,
+                        "resize": "fit"
+                     }
+                  }
+               }
+            ]
+         },
+         "metadata": {
+            "result_type": "popular",
+            "iso_language_code": "en"
+         },
+         "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+         "in_reply_to_status_id": null,
+         "in_reply_to_status_id_str": null,
+         "in_reply_to_user_id": null,
+         "in_reply_to_user_id_str": null,
+         "in_reply_to_screen_name": null,
+         "user": {
+            "id": 617155406,
+            "id_str": "617155406",
+            "name": "Steven Wilson",
+            "screen_name": "StevenWilsonHQ",
+            "location": "",
+            "description": "This is the official Twitter of musician, singer, songwriter and record producer Steven Wilson.",
+            "url": "https://t.co/G1ZbAQ0izd",
+            "entities": {
+               "url": {
+                  "urls": [
+                     {
+                        "url": "https://t.co/G1ZbAQ0izd",
+                        "expanded_url": "http://stevenwilsonhq.com",
+                        "display_url": "stevenwilsonhq.com",
+                        "indices": [
+                           0,
+                           23
+                        ]
+                     }
+                  ]
+               },
+               "description": {
+                  "urls": []
+               }
+            },
+            "protected": false,
+            "followers_count": 87118,
+            "friends_count": 25,
+            "listed_count": 762,
+            "created_at": "Sun Jun 24 14:14:45 +0000 2012",
+            "favourites_count": 4,
+            "utc_offset": null,
+            "time_zone": null,
+            "geo_enabled": false,
+            "verified": true,
+            "statuses_count": 1801,
+            "lang": null,
+            "contributors_enabled": false,
+            "is_translator": false,
+            "is_translation_enabled": false,
+            "profile_background_color": "0A0A0A",
+            "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+            "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+            "profile_background_tile": false,
+            "profile_image_url": "http://pbs.twimg.com/profile_images/1238011880582586368/pN69tR68_normal.jpg",
+            "profile_image_url_https": "https://pbs.twimg.com/profile_images/1238011880582586368/pN69tR68_normal.jpg",
+            "profile_banner_url": "https://pbs.twimg.com/profile_banners/617155406/1591372249",
+            "profile_link_color": "111111",
+            "profile_sidebar_border_color": "C0DEED",
+            "profile_sidebar_fill_color": "DDEEF6",
+            "profile_text_color": "333333",
+            "profile_use_background_image": true,
+            "has_extended_profile": false,
+            "default_profile": false,
+            "default_profile_image": false,
+            "following": null,
+            "follow_request_sent": null,
+            "notifications": null,
+            "translator_type": "none"
+         },
+         "geo": null,
+         "coordinates": null,
+         "place": null,
+         "contributors": null,
+         "is_quote_status": false,
+         "retweet_count": 19,
+         "favorite_count": 312,
+         "favorited": false,
+         "retweeted": false,
+         "possibly_sensitive": false,
+         "lang": "en"
+      }
+   ],
+   "search_metadata": {
+      "completed_in": 0.023,
+      "max_id": 1311671856303411202,
+      "max_id_str": "1311671856303411202",
+      "next_results": "?max_id=1310896758814396415&q=from%3AStevenWilsonHQ&count=2&include_entities=1&result_type=mixed",
+      "query": "from%3AStevenWilsonHQ",
+      "refresh_url": "?since_id=1311671856303411202&q=from%3AStevenWilsonHQ&result_type=mixed&include_entities=1",
+      "count": 2,
+      "since_id": 0,
+      "since_id_str": "0"
+   }
+}
---
+++ python-practice2.py
@@ -0,0 +1,17 @@
+dictionary = {
+  "name":"John",
+  "age":30,
+  "cars": [
+    { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+    { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+    { "name":"Fiat", "models":[ "500", "Panda" ] }
+  ]
+ }
+
+models = dictionary['cars'][2]['models']
+models.remove('Panda')
+try:
+    print(dictionary['cars'][2]['name']+ ' : ' + dictionary['cars'][2]['models'][1])
+except IndexError as identifier:
+    print(f"FATAL ERROR: {identifier}")
+
