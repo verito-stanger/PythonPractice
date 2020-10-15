0b6a26668506c572645d2251c916aa7fcbbd6f2d
Ortizmarijo <mariajose.o@geopagos.com>
2020-10-15 20:50:36 -0300

    practica3 if

--- Practice-1.py
+++ Practice-1.py
@@ -5,10 +5,15 @@ print(a+int(a))
 print(type(a))
 print(type(b))"""
 
-array = [23, 4 , 'Vero', 12.3,{'Vero': '95802122', 'Mikey': '19092838'}] 
-print(array[4]['Mikey'])
-array.append(2)
-array[4]['Mikey']= ['Modified', 'Unmodified']
+array = [23, 4 , 'Vero', 12.3,{'Vero': '95802122', 'Mikey': '19092838'},12.3, 'Azul', 12.3, 'Flor'] 
+"""print(array[4]['Mikey'])
+print(array[2]) 
+"""
+array.append('50000')
+array[4]= ['Modified', 'Unmodified']
 for a in array:
     print(a)
+    if(a==12.3):
+        print("Bienvenidos al Mundo")
+        continue
 
