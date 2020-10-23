62d39591fbd5e890fb121df511717add71ca9a99
Ortizmarijo <mariajose.o@geopagos.com>
2020-10-22 22:00:24 -0300

    While and for

--- Conditions.py
+++ Conditions.py
@@ -1,10 +1,10 @@
 #----Conditions----
 # Operators
 #Variables
-Integer = 10
+Integer = 0
 Float = 0
 String = "Mikey"
-Boolean = False
+Boolean = True
 Array = [1, 3, 4, 8, 76]
 Dictionary = {'Papagayo':'objeto', 'Patilla':'fruta'}
 Tuple = (12,25)
@@ -18,7 +18,7 @@ if (Boolean):
 else:
     print("Esa Vaina es FALSA!!")"""
 
-if(Float == 1):
+"""if(Float == 1):
     print("El Valor es muy bajo!!")
 elif (Float > 1 and Float < 5): #elif es un posible sustituto del switch case
     print("Todavia te falta mi ciela!")
@@ -31,7 +31,29 @@ if (String is not None):  #not None = No es Nulo
     print(String)
 
 if (len(Array2) != 0):
-    print("No hay nada adentro!")
+    print("No hay nada adentro!")"""
+"""numero = 100
+while Boolean:
+    numero = numero * 10
+    print(numero)
+    if (numero >= 100000):
+        Boolean = False
+        break """
+Nombres = []
+Documentos = []
+while (Integer <= 2):
+    Nombres.append(input ("Indroduzca su nombre:"))
+    Documentos.append(input("Indroduzca su numero de DNI:"))
+    print(f"se han registrado {Nombres[Integer]}")
+    Integer = Integer +1
+    continue
+Integer = 0
+for Nombre in Nombres:
+    print(Nombre)
+    print(Documentos[Integer])
+    Integer = Integer +1
+
+
 
 
 
