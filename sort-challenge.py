acdb8daa3f3eaf83e1d3ca77435cec5d1d694e9b
Mervin <mervindiazlugo>
2020-10-29 22:41:13 -0300

    sort Challenge

--- sort-challenge.py
+++ sort-challenge.py
@@ -1,12 +1,25 @@
 array = [34, 1 ,9 , 0 , 8, 21, 98, 23, 22, 67, 1230, 897, 7911, 1090, 256, 3500, 478, 21, 4, 2]
 #array.sort(reverse= True)
-print(array)
 sortedArray=[]
-i= 1
-menor= 0
-for lista in array:
-    if (lista <= array[i]):
-        menor=lista
-sortedArray.append(menor)
+
+delete = 0
+
+x = 0
+iteration = len(array) - 1
+while x <= iteration:
+    i = 0
+    x+=1
+    menor = array[0]
+    for lista in array:
+        if (lista <= menor):
+            menor=lista
+            delete = i
+        i+=1
+
+    del(array[delete])
+    sortedArray.append(menor)
+
+
+print(array)
 print(sortedArray)
         
